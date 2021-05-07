eea695f6dd5dfb8b0f19326395a2814f40430b6c
andy <kwakky1@gmail.com>
2021-05-07 18:16:18 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 5000
 
+const { User } = require('./model/User')
+
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({ extended:true }))
+
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://andykwak:andykwak@breezm.sl9ba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
@@ -12,6 +19,20 @@ mongoose.connect("mongodb+srv://andykwak:andykwak@breezm.sl9ba.mongodb.net/myFir
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.post('/register', ((req, res) => {
+    // 회원 가입 할때 필요한 정보들을 client 에서 가져오면
+    // 그것들을 클라이언트ㅔ
+
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+}))
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
